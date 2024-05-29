9b9632b083fa21c9d45bb1a2aff088c0b0b0006b
dominic <dominicseduraja3@gmail.com>
2024-05-29 23:53:24 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
-    <div className='bg-red-500'></div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
