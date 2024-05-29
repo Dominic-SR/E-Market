ae0a8f6c412eeb6955abfc3d0c764edb14dbcb2d
dominic <dominicseduraja3@gmail.com>
2024-05-29 23:45:50 +0530

    theme color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: "#f42c37",
+        secondary: "#f42c37",
+        brandYellow: "#fdc62e",
+        brandGreen: "#2dccef",
+        brandBlue: '#1376f4',
+        brandWhite: "#eeeeee",
+      }
+    },
   },  
   plugins: [],
 }
