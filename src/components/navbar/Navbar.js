399e187274bf01ca4db7fc3845413d1a2c669b86
dominic <dominicseduraja3@gmail.com>
2024-05-30 23:34:27 +0530

    nav bar added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='bg-white dark:bg-gray-900 dark:'>
+      <div>
+        <div className='container'>
+          <div>
+            <a
+            href='#'
+            className='text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl'
+            >
+              E Market
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
