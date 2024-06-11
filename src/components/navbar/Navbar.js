c4ef874ce310024cdec38bb6b4f83b125aec0b71
dominic <dominicseduraja3@gmail.com>
2024-06-11 20:46:13 +0530

    assets added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { IoMdSearch } from 'react-icons/io'
+import {FaCartShopping} from 'react-icons/fa6'
 
 function Navbar() {
 
@@ -61,8 +62,15 @@ function Navbar() {
               {/* Search bar section */}
                 <div className='relative group hidden sm:block'>
                   <input type='text' placeholder='Search' className='search-bar'/>
-                  <IoMdSearch className='text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute top-1/2 -translate-y-1 right-3' />
+                  <IoMdSearch className='text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200' />
                 </div>
+              {/* Order Button section */}
+              <button className='relative p-3'>
+                <FaCartShopping className='text-xl text-gray-600 dark:text-gray-400' />
+                <div className='w-4 h-4 bg-red-500 text-white rounded-full absolute top-0 right-0 flex items-center justify-center text-xs'>
+                  4
+                </div>
+              </button>
               {/* Dark mode section */}
                 <div></div>
             </div>
