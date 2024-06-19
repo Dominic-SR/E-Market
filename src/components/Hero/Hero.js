a5d277a15b836021606bd181e267c3a2e7b72df1
dominic <dominicseduraja3@gmail.com>
2024-06-19 22:55:54 +0530

    banner completed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Slider from "react-slick";
+import Button from '../Shared/Button';
 import Image1 from '../../assets/hero/headphone.png'
 import Image2 from '../../assets/hero/vr.png'
 import Image3 from '../../assets/hero/macbook.png'
@@ -44,7 +45,7 @@ const Hero = () => {
       };
   return (
     <div className='container'>
-        <div className='overflow-hidden rounded-3xl min-h-[550px] sm:min-h-[650px] hero-bg-color'>
+        <div className='overflow-hidden rounded-3xl min-h-[550px] sm:min-h-[650px] hero-bg-color flex justify-center items-center'>
             <div className='container pb-8 sm:pb-0'>
             {/* Hero Section */}
             <Slider {...settings}>
@@ -52,19 +53,23 @@ const Hero = () => {
                     <div key={data.id}>
                         <div className='grid grid-cols-1 sm:grid-cols-2'>
                             {/* Text Content Section */}
-                            <div>
-                                <h1>{data.subtitle}</h1>
-                                <h1>{data.title}</h1>
-                                <h1>{data.title2}</h1>
+                            <div className='flex flex-col justify-center gap-4 sm-pl-3 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10'>
+                                <h1 className='text-2xl sm:text-6xl lg:text-2xl font-bold'>{data.subtitle}</h1>
+                                <h1 className='text-5xl sm:text-5xl lg:text-7xl font-bold'>{data.title}</h1>
+                                <h1 className='text-5xl uppercase text-white dark:text-white/5 sm:text-[80px] md:text-[100px] xl:text-[150px] font-bold'>{data.title2}</h1>
                                 <div>
-                                    <button>Shop Now</button>
+                                    <Button
+                                        text="Shop Buy Category"
+                                        bgColor="bg-primary"
+                                        textColor="text-white"
+                                    />
                                 </div>
                             </div>
                             {/* Img Section */}
-                            <div>
+                            <div className='order-1 sm-order-2'>
                                 <div>
                                     <img src={data.img} alt="" 
-                                    className='w-[300px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6pxrgpa(0,0,0,.4)]'/>
+                                    className='w-[300px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6pxrgpa(0,0,0,.4)] relative z-40'/>
                                 </div>
                             </div>
                         </div>
