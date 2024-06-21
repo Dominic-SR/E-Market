31d42f4c1a2eadab1d59faee1e836e158b580247
dominic <dominicseduraja3@gmail.com>
2024-06-21 11:37:13 +0530

    gategory first line completed

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -29,19 +29,34 @@ const Category = () => {
             <div className='py-10 pl-5 bg-gradient-to-br from-brandYellow/90 to-brandYellow/90 text-white rounded-3xl relative h-[320px] flex items-end'>
                 <div className='mb-4'>
                     <div>
-                        <p className='mb=[2px] text-gray-400'>Enjoy</p>
+                        <p className='mb=[2px] text-white'>Enjoy</p>
                         <p className='text-2xl font-semibold mb-[2px]'>With</p>
-                        <p className='text-4xl xl:text-5xl font-bold opacity-20'>Earphone</p>
+                        <p className='text-4xl xl:text-5xl font-bold opacity-20'>Gadget</p>
                         <Button 
                             text="Browse"
-                            bgColor={"bg-primary"}
-                            texColor={"text-white"}
+                            bgColor={"bg-white"}
+                            textColor={"text-brandYellow"}
                         />
                     </div>
                 </div>
                 <img src={Image2} alt='' className='w-[320px] absolute -right-4 lg:top-[40px]' />
             </div>
             {/* third col */}
+            <div className='col-span-2 py-10 pl-5 bg-gradient-to-br from-primary to-primary/90 text-white rounded-3xl relative h-[320px] flex items-end'>
+                <div className='space-y-3'>
+                    <div className='mb-4'>
+                        <p className='mb=[2px] text-white'>Enjoy</p>
+                        <p className='text-2xl font-semibold mb-[2px]'>With</p>
+                        <p className='text-4xl xl:text-5xl font-bold opacity-40 mb-2'>Laptop</p>
+                        <Button 
+                            text="Browse"
+                            bgColor={"bg-white"}
+                            textColor={"text-primary"}
+                        />
+                    </div>
+                </div>
+                <img src={Image3} alt='' className='w-[250px] absolute top-1/2 -translate-y-1/2 -right-0' />
+            </div>
             </div>
         </div>
     </div>
