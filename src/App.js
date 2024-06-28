b0db36190ea76f01ab0e6a2d8fadcc6161049afe
dominic <dominicseduraja3@gmail.com>
2024-06-28 23:55:39 +0530

    add banner

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from './components/Hero/Hero'
 import Category from './components/Category/Category'
 import Category2 from './components/Category/Category2'
 import Services from './components/Services/Services'
+import Banner from './components/Banner/Banner'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Category />
       <Category2 />
       <Services />
+      <Banner />
     </div>
   )
 }
