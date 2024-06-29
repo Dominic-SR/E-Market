3f255ebae5ece90fe521b1fc2c6df3bc6cf2295e
dominic <dominicseduraja3@gmail.com>
2024-06-29 13:12:56 +0530

    add products

--- src/App.js
+++ src/App.js
@@ -5,6 +5,34 @@ import Category from './components/Category/Category'
 import Category2 from './components/Category/Category2'
 import Services from './components/Services/Services'
 import Banner from './components/Banner/Banner'
+import Products from './components/Products/Products'
+import Blogs from './components/Blogs/Blogs'
+
+import headphone from './assets/hero/headphone.png';
+import smartwatch2 from './assets/category/smartwatch2-removebg-preview.png'
+
+
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "10 Jam to 28 Jan",
+  image: headphone,
+  title2:"Air Solo Bass",
+  title3:"Winter Sale",
+  title4:"Loream ispum dolar sit amet cosecture",
+  bgColor:"#f42c37"
+}
+
+const BannerData2 = {
+  discount: "30% OFF",
+  title: "Happy Hours",
+  date: "10 Jam to 28 Jan",
+  image: smartwatch2,
+  title2:"Smart Solo",
+  title3:"Winter Sale",
+  title4:"Loream ispum dolar sit amet cosecture",
+  bgColor:"#2dcc6f"
+}
 
 function App() {
   return (
@@ -14,7 +42,10 @@ function App() {
       <Category />
       <Category2 />
       <Services />
-      <Banner />
+      <Banner data={BannerData}/>
+      <Products />
+      <Banner data={BannerData2}/>
+      <Blogs />
     </div>
   )
 }
