256875958aa7be04259e540d3e2e17de6075e6e2
Dominic-SR <dominicseduraja3@gmail.com>
2025-06-24 00:15:34 +0530

    navbar design

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import productData from "./assets/products.json"
+import Navbar from "./Components/Navbar/Navbar"
+import Sidebar from "./Components/Sidebar/Sidebar"
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div>
+      <Navbar />
+      <div>
+        <Sidebar />
+      </div>
+    </div>
   )
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='bg-red-500 p-4 shaow-md'>
+      <div className='max-w-7xl mx-auto flex justify-between items-center'>
+        <h1 className='text-white text-2xl font-serif font-normal'>E-Market</h1>
+        <input type="text" placeholder='Search for smartphones' className='p-3 text-base bg-white rounded w-1/2 focus:outline-none' />
+      </div>
+    </nav>
   )
 }
 
